07ef8fbbb0a9370c916ebfe31fd29d8dfbbe9259
Patrick Douglas <pdouglas.s@hotmail.com>
2023-10-22 11:41:27 -0300

    configurando prettier e eslint

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,7 @@
 import { Router } from 'express';
 
-
 const routes = new Router();
 
-routes.get('/teste', (req, res)=> {
-  return res.json({ ok: true });
-})
+routes.get('/teste', (req, res) => res.json({ ok: true }));
 
 export default routes;
